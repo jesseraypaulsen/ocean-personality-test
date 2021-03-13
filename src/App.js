c69f698b5782f19ec96843a74d80d831e35ec4cb
Jesse Paulsen <jesseraypaulsen@gmail.com>
2021-03-13 11:24:50 -0600

    calculateScore and getResult

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+const getResult = require('@alheimsins/b5-result-text');
+const calculateScore = require('@alheimsins/bigfive-calculate-score');
 
 function App() {
+
+  const data = {
+    "timeElapsed": -51,
+    "ip": "127.0.0.1",
+    "lang": "en",
+    "test": "50-IPIP-NEO-PI-R",
+    "totalQuestions": 50,
+    "answers": [
+      {
+        "domain": "A",
+        "facet": "1",
+        "score": "3"
+      },
+      {
+        "domain": "A",
+        "facet": "1",
+        "score": "3"
+      },
+      {
+        "domain": "E",
+        "facet": "1",
+        "score": "3"
+      },
+      {
+        "domain": "E",
+        "facet": "2",
+        "score": "3"
+      }
+    ]
+  }
+
+  //based on https://github.com/rubynor/bigfive-web/blob/master/api/result.js
+const scores = calculateScore(data);
+const results = getResult({scores, lang: 'en'})
+
+console.log(JSON.stringify(results, null, 2))
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {JSON.stringify(results, null, 2)}
       </header>
     </div>
   );
